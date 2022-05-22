186a9854d61f93a4aded1833a26f5064968cb838
Superpapocho <juan.russy@urosario.edu.co>
2022-05-22 18:59:40 -0500

    Dictionaries with information about stocks

---
+++ constantes_acciones.py
@@ -0,0 +1,40 @@
+# Diccionarios tickers
+
+# America
+_berkshire_hathaway_ = { 'TICKER_NAME': 'brk.a', 'exchange': 'nyse',  'screener': 'america'}
+_tesla_ = { 'TICKER_NAME' : 'tsla', 'exchange' : 'nasdaq', 'screener' : 'america' }
+_meta_ = { "TICKER_NAME" : 'fb', "exchange" : 'nasdaq', 'screener' : 'america' }
+_apple_ = { "TICKER_NAME" : 'aapl', "exchange" : 'nasdaq', 'screener' : 'america' }
+_pfizer_ = { "TICKER_NAME" : 'pfe', "exchange" : 'nyse', 'screener' : 'america' }
+_coca_cola_ = { "TICKER_NAME" : 'ko', "exchange" : 'nyse', 'screener' : 'america' }
+_oracle_ = { "TICKER_NAME" : 'orcl', "exchange" : 'nyse', 'screener' : 'america' }
+_master_card_ = { "TICKER_NAME" : 'ma', "exchange" : 'nyse', 'screener' : 'america' }
+_adobe_ = { "TICKER_NAME" : 'adbe', "exchange" : 'nasdaq', 'screener' : 'america' }
+_intel_ = { "TICKER_NAME" : 'docn', "exchange" : 'nasdaq', 'screener' : 'america' }
+_johnson_ = { "TICKER_NAME" : 'jnj', "exchange" : 'nyse', 'screener' : 'america' }
+_visa_ = { "TICKER_NAME" : 'v', "exchange" : 'nyse', 'screener' : 'america' }
+_xom_ = { "TICKER_NAME" : 'xom', "exchange" : 'nyse', 'screener' : 'america' }
+_disney_ = { "TICKER_NAME" : 'dis', "exchange" : 'nyse', 'screener' : 'america' }
+_netflix_ = { "TICKER_NAME" : 'nflx', "exchange" : 'nasdaq', 'screener' : 'america' }
+_cat_ = { "TICKER_NAME" : 'cat', "exchange" : 'nyse', 'screener' : 'america' }
+_paypal_ = { "TICKER_NAME" : 'pypl', "exchange" : 'nasdaq', 'screener' : 'america' }
+_microsoft_ = { "TICKER_NAME" : 'msft', "exchange" : 'nasdaq', 'screener' : 'america' }
+_amazon_ = { "TICKER_NAME" : 'amzn', "exchange" : 'nasdaq', 'screener' : 'america' }
+_nvidia_ = { "TICKER_NAME" : 'nvda', "exchange" : 'nasdaq', 'screener' : 'america' }
+_nvidia_ = { "TICKER_NAME" : 'pg', "exchange" : 'nyse', 'screener' : 'america' }
+_airbnb_ = { "TICKER_NAME" : 'abnb', "exchange" : 'nasdaq', 'screener' : 'america' }
+_amd_ = { "TICKER_NAME" : 'amd', "exchange" : 'nasdaq', 'screener' : 'america' }
+_ibm_ = { "TICKER_NAME" : 'ibm', "exchange" : 'nasdaq', 'screener' : 'america' }
+_mcdonalds_ = { "TICKER_NAME" : 'mcd', "exchange" : 'nyse', 'screener' : 'america' }
+_ford_ = { "TICKER_NAME" : 'f', "exchange" : 'nyse', 'screener' : 'america' }
+_hilton_ = { "TICKER_NAME" : 'hlt', "exchange" : 'nyse', 'screener' : 'america' }
+_boeing_ = { "TICKER_NAME" : 'ba', "exchange" : 'nyse', 'screener' : 'america' }
+_accenture_ = { "TICKER_NAME" : 'acn', "exchange" : 'nyse', 'screener' : 'america' }
+# Colombia
+_ecopetrol_ = { "TICKER_NAME" : 'ecopetrol', "exchange" : 'bvc', 'screener' : 'colombia' }
+_bancolombia_ = { "TICKER_NAME" : 'bcolombia', "exchange" : 'bvc', 'screener' : 'colombia' }
+_gruposura_ = { "TICKER_NAME" : 'gruposura', "exchange" : 'bvc', 'screener' : 'colombia' }
+_nutresa_ = { "TICKER_NAME" : 'nutresa', "exchange" : 'bvc', 'screener' : 'colombia' }
+_grupoaval_ = { "TICKER_NAME": 'grupoaval', "exchange": 'bvc', 'screener': 'colombia' }
+_exito_ = { "TICKER_NAME": 'exito', "exchange": 'bvc', 'screener': 'colombia' }
+_terpel_ = { "TICKER_NAME": 'terpel', "exchange": 'bvc', 'screener': 'colombia' }
